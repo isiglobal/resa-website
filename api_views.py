0ccef75bf163deeaefd67dc721187a6a7ed87c8f
Brandon Thomas <bt@brand.io>
2013-07-26 14:07:52 -0400

    Change title on server (WIP)

--- api_views.py
+++ api_views.py
@@ -4,6 +4,38 @@ from app import *
 # AJAX API GATEWAY
 # ----------------
 
+@app.route('/api/article/<url_key>',
+		methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+def api_article(url_key):
+	artId = url_key.split('-')[0] # TODO: Not safe. Error prone
+	article = None
+
+	if request.method == 'GET':
+		try:
+			article = database.session.query(Article) \
+						.filter_by(id=artId).one()
+		except:
+			pass
+		return json.dumps(article.serialize())
+
+	elif request.method in ['PUT', 'POST', 'PATCH']:
+		try:
+			article = database.session.query(Article) \
+						.filter_by(id=artId).one()
+			article.set_from_json(request.json)
+			database.session.commit()
+
+		except Exception as e:
+			print 'exception', e
+			return 'FAIL' # TODO FAILURE RESPONSE CODE
+
+		return json.dumps(article.serialize())
+
+
+# TODO: THIS IS DEPRECATED, I THINK
+# TODO: THIS IS DEPRECATED, I THINK
+# TODO: THIS IS DEPRECATED, I THINK
+# TODO: THIS IS DEPRECATED, I THINK
 @app.route('/api/articles', methods=['GET', 'POST', 'PATCH', 'PUT'])
 def api_articles():
 	if request.method == 'POST':
@@ -31,43 +63,4 @@ def api_articles():
 			return '{}'
 
 		return json.dumps([x.serialize() for x in articles])
-
-
-@app.route('/api/article/<url_key>',
-		methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
-def api_article(url_key):
-	artId = url_key.split('-')[0] # TODO: Not safe. Error prone
-	article = None
-
-	if request.method == 'GET':
-		try:
-			article = database.session.query(Article) \
-						.filter_by(id=artId).one()
-		except:
-			pass
-		return json.dumps(article.serialize())
-
-	elif request.method == 'PATCH':
-		print 'PATCH'
-		return 'TODO'
-		pass
-
-	elif request.method in ['PUT', 'POST']:
-		try:
-			article = database.session.query(Article) \
-						.filter_by(id=artId).one()
-
-			article.set_from_json(request.json)
-
-			#database.session.add(article)
-			database.session.commit()
-			return json.dumps(article.serialize())
-
-		except Exception as e:
-			print 'exception', e
-			return 'FAIL' # TODO FAILURE RESPONSE CODE
-		print request.json
-		print 'UPDATE'
-		return 'TODO'
-
 
--- js/article.js
+++ js/article.js
@@ -17,3 +17,54 @@ var Articles = Backbone.Collection.extend({
 	url: '/api/article',
 });
 
+var ArticleView = Backbone.View.extend({
+	events: {
+		'dblclick .title': 'editTitle',
+		'dblclick .content': 'editContent',
+	},
+	initialize: function() {
+		this.$el = $('#article');
+		//this.$edit = $('<textarea></textarea>');
+		this.listenTo(this.model, 'change', this.render);
+		this.delegateEvents();
+	},
+	render: function() {
+		console.log('render');
+		console.log('render');
+		console.log('render');
+		this.$el.find('.title').html(this.model.get('title'));
+		this.$el.find('.content').html(this.model.get('content_html'));
+	},
+	editTitle: function() {
+		var that = this,
+			$input = this.$el.find('.title_edit'),
+			$title = this.$el.find('.title');
+
+		$title.hide();
+		$input.show()
+			.focus()
+			.val($input.val())
+			.on('blur', function() {
+				console.log('blur event');
+				that.model.set('title', $(this).val());
+
+				// XXX: Backbone is stupid and expects attrib hash for CBs
+				that.model.save({
+					//title: $(this).val(),
+				}, {
+					//patch: true,
+					success: function(model, resp, opts) {
+						console.log('model.save() success!');
+						$input.hide();
+						$title.show();
+					},
+					failure: function(model, xhr, opts) {
+						console.log('model.save() error!');
+					},
+				});
+			});
+	},
+	editContent: function() {
+		console.log('edit content');
+	},
+});
